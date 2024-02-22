a5a39ca79ae37861990e713b38f891e548f42050
Xander Whitby <xwhitby20@gmail.com>
2024-02-21 22:04:03 -0700

    add notes to code, added resizing for background image and getting background image

--- flappybird.py
+++ flappybird.py
@@ -1,6 +1,24 @@
 import pygame
 pygame.init()
 
-TITLE = 'Flappy Bird'
-WIDTH = 400
-HEIGHT = 788
+
+#class for background for flappy bird
+class Background:
+    #this will have the image size as private attributes and return the image to the screen
+    def __init__(self, game_background):
+        self.game_background = game_background
+
+        return game_background
+
+    #this containst the background image
+    def get_image(self, background_image):
+        self.background_image = background_image
+
+        background_image = pygame.image.load('flappy_background.png')
+        width, height = pygame.image.get_size()
+
+
+    #this gets the size from the intialization; I don't need a private attribute here; use pygame to validate module for getting image size
+    def set_image_size(self, new_size):
+        self.new_size = new_size
+        new_size = pygame.transform.scale(image, (400, 788))
